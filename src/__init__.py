76103aa40ee5cfb52da54e4c6201088e8c97d8e2
Amiri-007 <149742134+Amiri-007@users.noreply.github.com>
2025-01-04 00:10:40 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 ###############################################################################
 # File: __init__.py
+# Author: Michael R. Amiri
+# Date: 2025-01-04
 #
 # Description:
 #  This is an empty file that indicates to Python that `src/` is a package.
